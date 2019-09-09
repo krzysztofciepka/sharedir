995cff65e10deedfa8230015b4158ca7d5af02ef
Krzysztof Ciepka <krzysztof.ciepka@blue-focus.com>
2019-09-09 12:25:13 +0200

    Use serve-index fork

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-const express = require('express');
-const serveIndex = require('serve-index');
-const natTunnel = require('nat-tunnel');
-const escapeHtml = require('escape-html');
 const crypto = require('crypto');
-const fs = require('fs');
-const path = require('path');
-const { iconStyle } = require('./icons');
-const { init } = require('./htmlUtils');
+const express = require('express');
+const natTunnel = require('nat-tunnel');
 
 const app = express();
 
@@ -18,49 +12,13 @@ module.exports = {
     // select some random port from 30000-40000 range
     const port = Math.round(Math.random() * (40000 - 30000) + 30000);
 
-    const htmlUtils = init(basePath);
+    const serveIndex = require('serve-index')(basePath);
 
     app.use(
       '/',
       express.static(process.cwd()),
       serveIndex(process.cwd(), {
         icons: true,
-        template: (locals, callback) => {
-          fs.readFile(
-            path.join(__dirname, 'public/template.html'),
-            { encoding: 'utf8' },
-            (err, data) => {
-              if (err) {
-                throw err;
-              }
-
-              let template = data;
-
-              template = template.replace(
-                /\{style\}/g,
-                locals.style.concat(
-                  iconStyle(locals.fileList, locals.displayIcons),
-                ),
-              ).replace(
-                /\{files\}/g,
-                htmlUtils.createHtmlFileList(
-                  locals.fileList,
-                  locals.directory,
-                  locals.displayIcons,
-                  locals.viewName,
-                ),
-              ).replace(
-                /\{directory\}/g,
-                escapeHtml(locals.directory),
-              ).replace(
-                /\{linked-path\}/g,
-                htmlUtils.htmlPath(locals.directory),
-              );
-
-              callback(null, template);
-            },
-          );
-        },
       }),
     );
 
